1c6977de60fd0913b34ba68a59cccfbef3595bda
ViGi02 <unathivayeke02@gmail.com>
2026-06-20 16:30:31 +0200

    Create authentication pages and setup Tailwind CSS

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>CareerNavigator AI</h1>
+
+      <form>
+        <div>
+          <label>Email</label>
+          <br />
+          <input type="email" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input type="password" />
+        </div>
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+
+      <p>
+        Don't have an account?{" "}
+        <Link to="/register">Register</Link>
+      </p>
     </div>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+
 function Register() {
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Create Account</h1>
+
+      <form>
+        <div>
+          <label>Name</label>
+          <br />
+          <input type="text" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input type="email" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input type="password" />
+        </div>
+
+        <br />
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+
+      <p>
+        Already have an account?{" "}
+        <Link to="/">Login</Link>
+      </p>
     </div>
   );
 }
